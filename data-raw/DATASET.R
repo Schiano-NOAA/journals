20bfd21af20e4db851f7ad39db7299f87dc0c8bf
Sam Schiano <125507018+Schiano-NOAA@users.noreply.github.com>
2026-08-27 17:01:09 -0400

    add foundation for script and ghaction to connect sis

---
+++ data-raw/DATASET.R
@@ -0,0 +1,10 @@
+## code to prepare `DATASET` dataset goes here
+
+# Call all reports in JSON
+
+# Function to write JSON to citations format for bib file
+json_to_bib <- function(json_data) {
+}
+
+# Save as bib file to inst
+
